2159b2420c372fb21ea987739504cda11eef4af9
ruslan_lekhman <lekhman-ruslan@i.ua>
2014-02-15 20:51:44 +0200

    mathjax init

--- src/js/directives.js
+++ src/js/directives.js
@@ -37,6 +37,23 @@ angular.module('symathApp.directives', [])
       }
     };
   }])
+  .directive('uiMathJax', ['$window', function($window) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var keyupHandler = function() {
+          if(element.val().length > 0) {
+            $('#' + attrs.uiMathJax).html('`' + element.val() + '`');
+            MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+          }
+          else
+            $('#' + attrs.uiMathJax).html('No data input.');
+        }
+        element.keyup(keyupHandler);
+        keyupHandler();
+      }
+    };
+  }])
   .directive('scrollSpy', ['$window', function($window) {
     return {
       restrict: 'A',
