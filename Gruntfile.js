77dd0fa9eb3305fe451ef1a8db70ad11b42f60c6
Denys Zariaiev <denys.zariaiev@gmail.com>
2014-02-27 22:33:55 +0200

    full cycle grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,38 @@
 'use strict';
  
 module.exports = function(grunt) {
+  
   grunt.loadNpmTasks('grunt-node-webkit-builder');
-  grunt.initConfig({
-    nodewebkit: {
-      options: {
-        build_dir: './build',
-        mac: false,
-        win: true,
-        linux32: true,
-        linux64: true,
-        version: '0.9.2'
-      },
-      src: ['./src/**/*']
-    }
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  
+  grunt.registerTask('npm-install', 'install the application dependencies', function() {
+    // adapted from http://www.dzone.com/snippets/execute-unix-command-nodejs
+    var exec = require('child_process').exec,
+        sys  = require('sys');
+
+    function puts(error, stdout, stderr) { console.log(stdout); sys.puts(stdout) }
+
+    // assuming this command is run from the root of the repo
+    exec('npm install', {cwd: './src'}, puts);
   });
-  grunt.registerTask('default', ['nodewebkit']);
+  
+  grunt.initConfig({ });
+  
+  grunt.config.set('nodewebkit', {
+    options: {
+      build_dir: './build',
+      mac: false,
+      win: true,
+      linux32: true,
+      linux64: true,
+      version: '0.9.1'
+    },
+    src: ['./src/**/*']
+  });
+  
+  grunt.config.set('clean', {
+    MathJax: [ 'src/lib/MathJax/fonts', 'src/lib/MathJax/unpacked' ]
+  });
+  
+  grunt.registerTask('default', ['npm-install', 'clean', 'nodewebkit']);
 };
